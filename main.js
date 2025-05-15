77e5c8fadce26a66b67b403715f6e45a6710e862
jpatrick5402 <jpatrick5402@gmail.com>
2025-05-15 08:46:35 -0400

    starting links

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-const linkDictionary = [
-    "https://github.com/",
-    "https://leetcode.com/",
-    "https://reddit.com/",
-]
-
 async function fetchWeather() {
     const weatherDiv = document.getElementById('weather')
     const url = `https://wttr.in/`
@@ -12,20 +6,7 @@ async function fetchWeather() {
     weatherDiv.innerHTML = `${data + `<style>pre {font-family: Source Code Pro !important; font-size: 12px;}</style>`}`
 }
 
-function generateLinks() {
-    const el = document.getElementById('links')
-    linkDictionary.forEach(url => {
-        const linkDiv = document.createElement('div')
-        const hyperlink = document.createElement('a')
-        hyperlink.href = url
-        hyperlink.innerHTML = url
-        linkDiv.appendChild(hyperlink)
-        el.appendChild(linkDiv)
-    })
-}
-
 window.onload = () => {
     document.getElementById('google-search-input').focus()
     fetchWeather()
-    generateLinks()
 }
