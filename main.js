f19fd3952947c05254ef9b0f3841e9f8fa8ae105
jpatrick5402 <jpatrick5402@gmail.com>
2025-05-16 18:55:42 -0400

    Adding error handling

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 async function fetchWeather() {
-    const options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0,
-    };
-
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(async function(position) {
             const weatherDiv = document.getElementById('weather')
-            const url = `https://wttr.in/${position.coords.latitude},${position.coords.longitude}`
+            const url = `https://wttr.in/${position.coords.latitude.toPrecision(4)},${position.coords.longitude.toPrecision(4)}`
+            console.log(position.coords.latitude.toPrecision(4))
             const res = await fetch(url)
             const data = await res.text()
             weatherDiv.innerHTML = `${data + `<style>pre {font-family: Source Code Pro !important; font-size: 12px;}</style>`}`
+        },
+        function(err) {
+            console.log('An error has occurred while fetching the weather')
+            console.log(err)
         });
     } else {
         console.error("Geolocation is not supported by this browser.");
