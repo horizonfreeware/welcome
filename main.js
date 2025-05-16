f6e6b74117726ed7458ed96417fd69f005507d9a
jpatrick5402 <jpatrick5402@gmail.com>
2025-05-16 07:45:50 -0400

    More accurate weather

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 async function fetchWeather() {
-    const weatherDiv = document.getElementById('weather')
-    const url = `https://wttr.in/`
-    const res = await fetch(url)
-    const data = await res.text()
-    weatherDiv.innerHTML = `${data + `<style>pre {font-family: Source Code Pro !important; font-size: 12px;}</style>`}`
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    };
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async function(position) {
+            const weatherDiv = document.getElementById('weather')
+            const url = `https://wttr.in/${position.coords.latitude},${position.coords.longitude}`
+            const res = await fetch(url)
+            const data = await res.text()
+            weatherDiv.innerHTML = `${data + `<style>pre {font-family: Source Code Pro !important; font-size: 12px;}</style>`}`
+        });
+    } else {
+        console.error("Geolocation is not supported by this browser.");
+    }
 }
 
 window.onload = () => {
